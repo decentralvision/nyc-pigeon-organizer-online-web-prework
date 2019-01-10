1a76cae5c0bdf897d1605e0a4bd37c736c2ed6ff
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-10 21:52:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  output = {}
+  #create hash with names as keys to data hashees
+  names_arr = []
+  data[:gender].each do |gender, names|
+    names.each{|gender, name| names_arr.push(name)}
+    binding.pry
+  end
+  names_arr.each {|name| output[name] = {}}
+  names_arr.each do |name|
+    puts name
+    data.each do |attribute, attr_type|
+      binding.pry
+    end
+  end
+  puts output
+  output
 end
